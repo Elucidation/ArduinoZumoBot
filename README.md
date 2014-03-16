15f025bec7e191c397bd68b0e2ec7a9f6ca75870
Sameer Ansari <elucidation@gmail.com>
2014-03-16 19:11:31 -0400

    prep for pan/tilt

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
 ArduinoZumoBot
 ==============
 
-Arduino code for controlling a custom-built pololu Zumo bot with encoder(s) and xbee remote serial
+Arduino code for controlling a custom-built pololu Zumo bot with encoder(s) 
+and a pan/tilt servo setup.
+
+Communicates over rosserial.
+
+
+Wiring
+---
+
+In Order:
+
+```
+2  - Right Encoder A
+3  - Buzzer
+4  - Left Encoder A
+5  - Servo Pan
+6  - Servo Tilt
+7  - Right Motor Direction
+8  - Left Motor Direction
+9  - Right Motor Speed
+10 - Left Motor Speed
+11 - Right Encoder B
+12 - User Pushbutton
+13 - LED
+
+A0 -
+A1 - Battery Voltage
+A2 - Left Encoder B
+A3 -
+A4/SDA - I2C 3-axis compass module
+A5/SCL - I2C 3-axis compass module
+```
+
+In parts:
+
+```
+5  - Servo Pan
+6  - Servo Tilt
+
+7  - Right Motor Direction
+8  - Left Motor Direction
+9  - Right Motor Speed
+10 - Left Motor Speed
+
+12 - User Pushbutton
+13 - LED
+
+2  - Right Encoder A
+11 - Right Encoder B
+
+4  - Left Encoder A
+A2 - Left Encoder B
+
+A1 - Battery Voltage
+
+A4/SDA - I2C 3-axis compass module
+A5/SCL - I2C 3-axis compass module
+```
--- controller/controller.ino
+++ controller/controller.ino
@@ -7,49 +7,40 @@
 // Distance between both wheels
 #define WHEEL_OFFSET_M 0.1 // 10cm
 
-#define LED_PIN 13
 #define MAX_REAL_SPEED 0.65 // m/s
 #define MAX_MOTOR_SPEED 400 // RPM
 
-// Battery Voltage
-#define BATTERY_READ_PIN 1
+// Others
+#define BATTERY_READ_PIN 1 // (SPECIAL) Analog Pin A1
+#define USER_PUSHBUTTON_PIN 12
+#define LED_PIN 13
 
-ZumoMotors motors;
+// Encoders
+#define ENCODER_LEFT_PIN_A 2
+#define ENCODER_LEFT_PIN_B 11
 
-// String inputString = "";         // a string to hold incoming data
-// boolean stringComplete = false;  // whether the string is complete
-Timer t;
-
-/// Encoder
-int encoder0PinA = 2;
-int encoder0PinB = 11;
-int encoder0Pos = 0;
-int encoder0PinALast = LOW;
-volatile int n = LOW;
-int last = 0;
-
-int encoder1PinA = 5;
-int encoder1PinB = A2;
-int encoder1Pos = 0;
-int encoder1PinALast = LOW;
-volatile int n2 = LOW;
-int last1 = 0;
+#define ENCODER_RIGHT_PIN_A 4
+#define ENCODER_RIGHT_PIN_B 2 // (SPECIAL) Analog pin A2
+#define ENCODER_RIGHT_PIN_B_THRESHOLD 450 // Read value threshold crossing
 
 
-ros::NodeHandle  nh;
+// ROS
+ros::NodeHandle nh;
 geometry_msgs::Twist twist_msg;
 
+ZumoMotors motors; // Motor controller
+Timer t;
 
-float getBatteryVoltage() {
-  return batteryVoltage = 5.0 * analogRead(BATTERY_READ_PIN) / 1024.0;
-}
 
-void cmd_vel_callback( const geometry_msgs::Twist& twist_msg) {
-  diff_drive(twist_msg.linear.x, twist_msg.angular.z);
-}
+/// Left Encoder
+int encoder_left_pos = 0;
+int encoder_left_pin_A_last = LOW;
+volatile int encoder_left_val = LOW;
 
-// Command Velocity Subscriber
-ros::Subscriber<geometry_msgs::Twist> cmd_vel_topic("cmd_vel", &cmd_vel_callback);
+// Right Encoder
+int encode_right_pos = 0;
+int encoder_right_pin_A_last = LOW;
+volatile int encoder_right_val = LOW;
 
 // Battery Voltage Publisher
 std_msgs::Float32 voltage_msg;
@@ -61,57 +52,16 @@ void publishBatteryVoltage() {
 }
 
 
-void measureLeftEncoder()
-{
-    n = digitalRead(encoder1PinA);
-    if ((encoder1PinALast == LOW) && (n == HIGH))
-    {
-        if (analogRead(encoder1PinB) < 450)
-        {
-            encoder1Pos--;
-        }
-        else
-        {
-            encoder1Pos++;
-        }
-    }
-    encoder1PinALast = n;
+// Command Velocity Callback
+void cmd_vel_callback( const geometry_msgs::Twist& twist_msg) {
+  diff_drive(twist_msg.linear.x, twist_msg.angular.z);
 }
 
-void measureRightEncoder()
-{
-    n = digitalRead(encoder0PinA);
-    if ((encoder0PinALast == LOW) && (n == HIGH))
-    {
-        if (digitalRead(encoder0PinB) == LOW)
-        {
-            //if (analogRead(encoder0PinB) < 450) {
-            encoder0Pos--;
-        }
-        else
-        {
-            encoder0Pos++;
-        }
-    }
-    encoder0PinALast = n;
-}
-void printStuff()
-{
-    if (last != encoder0Pos)
-    {
-        // Serial.print ("A");
-        // Serial.println (encoder0Pos);
-        last = encoder0Pos;
-    }
+// Command Velocity Subscriber
+ros::Subscriber<geometry_msgs::Twist> cmd_vel_topic("cmd_vel", &cmd_vel_callback);
 
-    if (last1 != encoder1Pos)
-    {
-        // Serial.print ("B");
-        // Serial.println (encoder1Pos);
-        last1 = encoder1Pos;
-    }
-}
-///
+
+////////////////////////////////////////////////////////////////////////////////
 
 void setup()
 {
@@ -123,14 +73,13 @@ void setup()
   pinMode(LED_PIN, OUTPUT);
   
   // Initialize Encoders
-  pinMode (encoder0PinA, INPUT);
-  pinMode (encoder0PinB, INPUT);
-  pinMode (encoder1PinA, INPUT);
-  // pinMode (encoder1PinB, INPUT); // Analog
-  attachInterrupt(0, measureRightEncoder, CHANGE);
+  pinMode (ENCODER_LEFT_PIN_A, INPUT);
+  pinMode (ENCODER_LEFT_PIN_B, INPUT);
+  pinMode (ENCODER_RIGHT_PIN_A, INPUT);
+  // pinMode (encoder_right_pin_B, INPUT); // Analog
   
-  // Print encoder values every second
-  // t.every(1000, printStuff);
+  // Only Right encoder works with interrupts (left pinB doesn't pass interrupt threshold of ~3.3V)
+  attachInterrupt(0, measureRightEncoder, CHANGE); // 0 = pin 2 interrupt
 
   // Publish battery voltage every second
   t.every(1000, publishBatteryVoltage);
@@ -139,8 +88,7 @@ void setup()
   motors.flipLeftMotor(true);
   
   // Initial speed is zero
-  motors.setLeftSpeed(0);
-  motors.setRightSpeed(0);
+  drive_stop();
 
   nh.initNode();
   nh.subscribe(cmd_vel_topic);
@@ -157,6 +105,7 @@ void loop()
   nh.spinOnce();
 }
 
+////////////////////////////////////////////////////////////////////////////////
 
 // cmd_x  : linear x velocity (forward velocity)
 // cmd_th : angular z velocity (rotation of heading) 
@@ -168,14 +117,42 @@ void diff_drive(double cmd_x, double cmd_th) {
 }
 
 // Drive left and right wheel motors for dt milliseconds, blocking
-void drive(int vl, int vr, int dt_millis) {
+void drive(int vl, int vr) {
   motors.setLeftSpeed(vl);
   motors.setRightSpeed(vr);
-  t.after(dt_millis, drive_stop);
 }
 
 // Stops motors
 void drive_stop() {
   motors.setLeftSpeed(0);
   motors.setRightSpeed(0);
+}
+
+// Battery Voltage
+float getBatteryVoltage() {
+  return 5.0 * analogRead(BATTERY_READ_PIN) / 1024.0;
+}
+
+// Encoders
+void measureLeftEncoder()
+{
+    encoder_left_val = digitalRead(ENCODER_RIGHT_PIN_A);
+    if ((encoder_right_pin_A_last == LOW) && (encoder_left_val == HIGH))
+    {
+        if (analogRead(ENCODER_RIGHT_PIN_B) < ENCODER_RIGHT_PIN_B_THRESHOLD) 
+          {encode_right_pos--;}
+        else {encode_right_pos++;}
+    }
+    encoder_right_pin_A_last = encoder_left_val;
+}
+
+void measureRightEncoder()
+{
+    encoder_right_val = digitalRead(ENCODER_LEFT_PIN_A);
+    if ((encoder_left_pin_A_last == LOW) && (encoder_right_val == HIGH))
+    {
+        if (digitalRead(ENCODER_LEFT_PIN_B) == LOW) {encoder_left_pos--;}
+        else {encoder_left_pos++;}
+    }
+    encoder_left_pin_A_last = encoder_right_val;
 }
