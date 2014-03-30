4945b4328d8d241f9cb1bfe2f63c60a648eeb374
Elucidation <elucidation@gmail.com>
2014-03-30 03:00:26 -0400

    added compass tracking, but arduino uno hasn't got enough memory

--- controller/controller.ino
+++ controller/controller.ino
@@ -5,6 +5,16 @@
 #include <ZumoMotors.h>
 #include <geometry_msgs/Twist.h>
 #include <std_msgs/Float32.h>
+#include <std_msgs/Int32.h>
+
+// Compass
+#include <Wire.h> // To communicate with compass over i2c
+#include <LSM303.h>
+
+#define CALIBRATION_SAMPLES 70  // Number of compass readings to take when calibrating
+#define CRB_REG_M_2_5GAUSS 0x60 // CRB_REG_M value for magnetometer +/-2.5 gauss full scale
+#define CRA_REG_M_220HZ    0x1C // CRA_REG_M value for magnetometer 220 Hz update rate
+
 
 // PID controllers for wheels
 // #include <PID_v1.h> // Couldn't use, ran out of memory, using function-based PID
@@ -47,6 +57,17 @@ ros::NodeHandle nh;
 geometry_msgs::Twist twist_msg;
 
 ZumoMotors motors; // Motor controller
+
+LSM303 compass;
+
+// Compass heading publisher
+std_msgs::Float32 compass_heading;
+ros::Publisher compass_pub("heading", &compass_heading);
+
+void publishCompassHeading() {
+  compass_heading.data = getCompassReading();
+  compass_pub.publish( &compass_heading );
+}
 
 // Left Encoder
 long encoder_left_ticks = 0;
@@ -100,12 +121,25 @@ void publishBatteryVoltage() {
   battery_pub.publish( &voltage_msg );
 }
 
-
 // Command Velocity Callback
 void cmd_vel_callback( const geometry_msgs::Twist& twist_msg) {
   // messages in meters per second
   diff_drive(twist_msg.linear.x, twist_msg.angular.z);
 }
+
+// State Callback
+int state = 0; // 0 - safe, 1 - calibrate compass, 2 - normal
+std_msgs::Int32 state_msg;
+void state_callback( const std_msgs::Int32& state_msg) {
+  if (state_msg.data == 1) {
+    state = 1;
+    calibrateCompass();
+    state = 0;
+  }
+}
+
+// State Subscriber
+ros::Subscriber<std_msgs::Int32> state_topic("state", &state_callback);
 
 // Command Velocity Subscriber
 ros::Subscriber<geometry_msgs::Twist> cmd_vel_topic("cmd_vel", &cmd_vel_callback);
@@ -116,6 +150,9 @@ unsigned long last_battery_published = 0;
 void setup()
 {  
   pinMode(LED_PIN, OUTPUT);
+
+  // Initialize Compass
+  setupCompass();
   
   // Initialize Encoders
   pinMode (ENCODER_LEFT_PIN_A, INPUT);
@@ -139,11 +176,13 @@ void setup()
   nh.getHardware()->setBaud(ROS_SERIAL_BAUD_RATE);
   nh.initNode();
   nh.advertise(battery_pub); // Battery Voltage Publisher
+  nh.advertise(compass_pub); // Compass Publisher
 
   // Encoder publishers
   nh.advertise(lwheel_pub);
   nh.advertise(rwheel_pub);
 
+  nh.subscribe(state_topic); // Robot State subscriber
   nh.subscribe(cmd_vel_topic); // Command Velocity Subscriber
 
   last_battery_published = millis();
@@ -155,6 +194,7 @@ void loop()
   if (millis() - last_battery_published > BATTERY_VOLTAGE_PUBLISH_RATE)
   {
     publishBatteryVoltage();
+    if (state >= 2) publishCompassHeading();
     last_battery_published = millis();
   }
 
@@ -322,4 +362,97 @@ void doRightPID()
   
     /* Remember some variables for next time */
     right_lastInput = pid_right_input;
+}
+
+void setupCompass()
+{
+  // The highest possible magnetic value to read in any direction is 2047
+  // The lowest possible magnetic value to read in any direction is -2047
+  LSM303::vector<int16_t> running_min = {32767, 32767, 32767}, running_max = {-32767, -32767, -32767};
+
+  // Initiate the Wire library and join the I2C bus as a master
+  Wire.begin();
+
+  // Initiate LSM303
+  compass.init();
+
+  // Enables accelerometer and magnetometer
+  compass.enableDefault();
+
+  compass.writeReg(LSM303::CRB_REG_M, CRB_REG_M_2_5GAUSS); // +/- 2.5 gauss sensitivity to hopefully avoid overflow problems
+  compass.writeReg(LSM303::CRA_REG_M, CRA_REG_M_220HZ);    // 220 Hz compass update rate
+}
+
+
+// Will spin in place, overriding cmd_vel etc.
+void calibrateCompass()
+{
+    LSM303::vector<int16_t> running_min = {32767, 32767, 32767}, running_max = {-32767, -32767, -32767};
+
+  // To calibrate the magnetometer, the Zumo spins to find the max/min
+  // magnetic vectors. This information is used to correct for offsets
+  // in the magnetometer data.
+  motors.setLeftSpeed(200);
+  motors.setRightSpeed(-200);
+
+  for(unsigned char index = 0; index < CALIBRATION_SAMPLES; index ++)
+  {
+    // Take a reading of the magnetic vector and store it in compass.m
+    compass.read();
+
+    running_min.x = min(running_min.x, compass.m.x);
+    running_min.y = min(running_min.y, compass.m.y);
+
+    running_max.x = max(running_max.x, compass.m.x);
+    running_max.y = max(running_max.y, compass.m.y);
+
+    delay(50);
+  }
+
+  // Stop spinning
+  motors.setLeftSpeed(0);
+  motors.setRightSpeed(0);
+
+  // Set calibrated values to compass.m_max and compass.m_min
+  compass.m_max.x = running_max.x;
+  compass.m_max.y = running_max.y;
+  compass.m_min.x = running_min.x;
+  compass.m_min.y = running_min.y;
+
+  state = 2; // Set to next state
+}
+
+// Converts x and y components of a vector to a heading in degrees.
+// This function is used instead of LSM303::heading() because we don't
+// want the acceleration of the Zumo to factor spuriously into the
+// tilt compensation that LSM303::heading() performs. This calculation
+// assumes that the Zumo is always level.
+template <typename T> float heading(LSM303::vector<T> v)
+{
+  float x_scaled =  2.0*(float)(v.x - compass.m_min.x) / ( compass.m_max.x - compass.m_min.x) - 1.0;
+  float y_scaled =  2.0*(float)(v.y - compass.m_min.y) / (compass.m_max.y - compass.m_min.y) - 1.0;
+
+  float angle = atan2(y_scaled, x_scaled)*180 / M_PI;
+  if (angle < 0)
+    angle += 360;
+  return angle;
+}
+
+// Average 10 vectors to get a better measurement and help smooth out
+// the motors' magnetic interference.
+float getCompassReading()
+{
+  LSM303::vector<int32_t> avg = {0, 0, 0};
+
+  for(int i = 0; i < 10; i ++)
+  {
+    compass.read();
+    avg.x += compass.m.x;
+    avg.y += compass.m.y;
+  }
+  avg.x /= 10.0;
+  avg.y /= 10.0;
+
+  // avg is the average measure of the magnetic vector.
+  return heading(avg);
 }
